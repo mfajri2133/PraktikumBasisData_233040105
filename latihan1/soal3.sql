961fc5da11d29bad7b258ae20349e4abfceef05a
Fajri <spag.959@gmail.com>
2024-10-09 13:50:52 +0700

    perubahan soal no 3

--- latihan1/soal3.sql
+++ latihan1/soal3.sql
@@ -1,8 +1,8 @@
 SELECT C.custid, C.companyname, O.orderid, O.orderdate 
 FROM Sales.Customers AS C   
 INNER JOIN Sales.Orders AS O     
-ON C.custid = O.custid;
-
+ON C.custid = O.custid
+ORDER BY c.custid;
 
 ---Query ini menampilkan kolom customer id dari table sales.customers, kolom nama perusahaan dari table sales.customers---
 ---kolom order id dari table sales.orders dan kolom tanggal pemesanan dari table sales.orders lalu dicocokan kedua table tersebut berdasarkan customer id nya----
