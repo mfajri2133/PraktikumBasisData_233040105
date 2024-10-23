737ec753f3f260a789091a536a9e1a666420fd4f
Fajri <spag.959@gmail.com>
2024-10-23 10:38:00 +0700

    perubahan

--- pertemuan5/tugas3/soal7.sql
+++ pertemuan5/tugas3/soal7.sql
@@ -1,2 +1,10 @@
+
+---Jika kita di awal belum setting on delete cascade maka harus diubah ter;ebih dahulu-
+ALTER TABLE Mahasiswa
+ADD CONSTRAINT fk_dosen_wali
+FOREIGN KEY (dosen_wali_id) REFERENCES Dosen_Wali(id) ON DELETE CASCADE;
+
+
+--Baru dilakukan penghapusan--
 DELETE FROM mahasiswa
 WHERE NPM IN (20061001, 20061002, 20061003, 20061004, 20061005);
